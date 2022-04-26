b1a24d3953222d5fa8d81178a8adf8836560e19f
fsnhsk <kangyu@utexas.edu>
2022-04-26 16:10:51 -0700

    tweet-photo-api

--- tweets/models.py
+++ tweets/models.py
@@ -36,14 +36,17 @@ class TweetPhoto(models.Model):
     # 图片在哪个 Tweet 下面
     tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
 
-
+    # 谁上传了这张图片，这个信息虽然可以从 tweet 中获取到，但是重复的记录在 Image 里可以在
+    # 使用上带来很多遍历，比如某个人经常上传一些不合法的照片，那么这个人新上传的照片可以被标记
+    # 为重点审查对象。或者我们需要封禁某个用户上传的所有照片的时候，就可以通过这个 model 快速
+    # 进行筛选
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-
+    # 图片文件
     file = models.FileField()
     order = models.IntegerField(default=0)
 
-
+    # 图片状态，用于审核等情况
     status = models.IntegerField(
         default=TweetPhotoStatus.PENDING,
         choices=TWEET_PHOTO_STATUS_CHOICES,
