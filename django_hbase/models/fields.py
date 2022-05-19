c2a6010a5c033eec23fa010593d666b3adab7b35
kangy <kangyu@utexas.edu>
2022-05-18 20:52:44 -0700

    initial-django-hbase

---
+++ django_hbase/models/fields.py
@@ -0,0 +1,21 @@
+class HbaseField:
+    field_type = None
+
+    # optional.
+    def __init__(self, reverse=False, column_family=None):
+        self.reverse = reverse
+        self.column_family = column_family
+
+
+class IntegerField(HbaseField):
+    field_type = 'int'
+    #optional.
+    def __init__(self, *args, auto_now_add=False, **kwargs):
+        super(IntegerField, self).__init__( *args, **kwargs)
+
+
+class TimestampField(HbaseField):
+    field_type = 'timestamp'
+    #optional.
+    def __init__(self, *args, auto_now_add=False, **kwargs):
+        super(TimestampField, self).__init__( *args, **kwargs)
